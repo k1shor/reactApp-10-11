ab06e0500da57fc85356e948df650215af5df54a
k1shor <k1shor@hotmail.com>
2022-04-27 11:34:54 +0545

    sorted products

---
+++ src/Components/product/Products_in_Users_Page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Products_in_Users_Page = () => {
+  return (
+    <div>Products_in_Users_Page</div>
+  )
+}
+
+export default Products_in_Users_Page
--- src/Components/product/productAPI.js
+++ src/Components/product/productAPI.js
@@ -20,9 +20,18 @@ export const addProduct = (product) => {
 export const getAllProducts = () => {
     return fetch(`${API}/showProducts`,{
         method:"GET",
-        headers:{
-            Accept:'application/json'
-        }
+        // headers:{
+        //     Accept:'application/json'
+        // }
+    })
+    .then(res=>res.json())
+    .catch(err=>console.log(err))
+}
+
+//to get sorted products
+export const getSortedProducts =(sortBy,order,limit) => {
+    return fetch(`${API}/showProducts?sortBy=${sortBy}&order=${order}&limit=${limit}`,{
+        method:"GET"
     })
     .then(res=>res.json())
     .catch(err=>console.log(err))
